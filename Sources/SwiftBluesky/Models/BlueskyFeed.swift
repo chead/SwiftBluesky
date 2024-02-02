287f523b9f08efc5a18ef1d3885ae1e0f32de721
chead <chead@chead.net>
2024-02-01 20:30:29 -0800

    Removed extraneous embed cases.

--- Sources/SwiftBluesky/Models/BlueskyFeed.swift
+++ Sources/SwiftBluesky/Models/BlueskyFeed.swift
@@ -188,13 +188,13 @@ public struct BlueskyFeedNotFoundPost: Decodable {
 
 public enum BlueskyFeedPostViewEmbedType: Decodable {
     private enum FieldType: String, Decodable {
-        case blueskyEmbedImages = "app.bsky.embed.images"
+//        case blueskyEmbedImages = "app.bsky.embed.images"
         case blueskyEmbedImagesView = "app.bsky.embed.images#view"
-        case blueskyEmbedExternal = "app.bsky.embed.external"
+//        case blueskyEmbedExternal = "app.bsky.embed.external"
         case blueskyEmbedExternalView = "app.bsky.embed.external#view"
-        case blueskyEmbedRecord = "app.bsky.embed.record"
+//        case blueskyEmbedRecord = "app.bsky.embed.record"
         case blueskyEmbedRecordView = "app.bsky.embed.record#view"
-        case blueskyEmbedRecordWithMedia = "app.bsky.embed.recordWithMedia"
+//        case blueskyEmbedRecordWithMedia = "app.bsky.embed.recordWithMedia"
         case blueskyEmbedRecordWithMediaView = "app.bsky.embed.recordWithMedia#view"
     }
 
@@ -202,13 +202,9 @@ public enum BlueskyFeedPostViewEmbedType: Decodable {
         case type = "$type"
     }
 
-    case blueskyEmbedImages(BlueskyEmbedImages)
     case blueskyEmbedImagesView(BlueskyEmbedImagesView)
-    case blueskyEmbedExternal(BlueskyEmbedExternal)
     case blueskyEmbedExternalView(BlueskyEmbedExternalView)
-    case blueskyEmbedRecord(BlueskyEmbedRecord)
     case blueskyEmbedRecordView(BlueskyEmbedRecordView)
-    case blueskyEmbedRecordWithMedia(BlueskyEmbedRecordWithMedia)
     case blueskyEmbedRecordWithMediaView(BlueskyEmbedRecordWithMediaView)
 
     public init(from decoder: Decoder) throws {
@@ -219,26 +215,14 @@ public enum BlueskyFeedPostViewEmbedType: Decodable {
         let singleValueContainer = try decoder.singleValueContainer()
 
         switch fieldType {
-        case .blueskyEmbedImages:
-            try self = .blueskyEmbedImages(singleValueContainer.decode(BlueskyEmbedImages.self))
-
         case .blueskyEmbedImagesView:
             try self = .blueskyEmbedImagesView(singleValueContainer.decode(BlueskyEmbedImagesView.self))
-
-        case .blueskyEmbedExternal:
-            try self = .blueskyEmbedExternal(singleValueContainer.decode(BlueskyEmbedExternal.self))
 
         case .blueskyEmbedExternalView:
             try self = .blueskyEmbedExternalView(singleValueContainer.decode(BlueskyEmbedExternalView.self))
 
-        case .blueskyEmbedRecord:
-            try self = .blueskyEmbedRecord(singleValueContainer.decode(BlueskyEmbedRecord.self))
-
         case .blueskyEmbedRecordView:
             try self = .blueskyEmbedRecordView(singleValueContainer.decode(BlueskyEmbedRecordView.self))
-
-        case .blueskyEmbedRecordWithMedia:
-            try self = .blueskyEmbedRecordWithMedia(singleValueContainer.decode(BlueskyEmbedRecordWithMedia.self))
 
         case .blueskyEmbedRecordWithMediaView:
             try self = .blueskyEmbedRecordWithMediaView(singleValueContainer.decode(BlueskyEmbedRecordWithMediaView.self))
